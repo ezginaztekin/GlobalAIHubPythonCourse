8273fa143796f9da2f6389ec62253c3cea34df17
ezginaztekin <80275365+ezginaztekin@users.noreply.github.com>
2021-03-14 22:00:50 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,1 +1,12 @@
+oddnumbers=[1,3,5,7,9,11,13,15,17,19,21]
+evennumbers=[0,2,4,6,8,10,12,14,16,18,20]
 
+list= evennumbers+oddnumbers
+list.sort()
+print(list)
+
+newlist=[i*2 for i in list]
+print(newlist)
+
+for x in newlist:
+    print(type(x))
