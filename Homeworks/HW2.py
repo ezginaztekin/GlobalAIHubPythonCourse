5f8c77634237818d63bd0c8a7f98763c5c632ddc
ezginaztekin <80275365+ezginaztekin@users.noreply.github.com>
2021-03-14 22:01:17 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,1 +1,11 @@
+cv1={"name": "Ezgi Naz","surname": "Tekin","age" : "25","job":"Master student", "school": "ODTÜ" }
+cv2={"name": "Ahmet","surname": "Yılmaz","age" : "28","job":"Civil Engineer", "school": "Boğaziçi University" }
+cv3={"name": "Berna","surname": "Işık","age" : "22","job":"student", "school": "İTÜ" }
+cv4={"name": "Elif","surname": "Gültekin","age" : "24","job":"Architect", "school": "Bilkent University" }
+cv5={"name": "Ali","surname": "Bayram","age" : "23","job":"Computer Engineer", "school": "Bahçeşehir University" }
 
+print(cv1)
+print(cv2)
+print(cv3)
+print(cv4)
+print(cv5)
